0283e10178d6d5eba43124251efd2efa9aefd877
Pascal-Mac <pascalsbakker@gmail.com>
2018-02-26 12:55:46 -0500

    Modified Main Page for the app.
    Added a new cardinal main card

--- Mobile/VenezuelaDreams/VenezuelaDreams/MainViewController.swift
+++ Mobile/VenezuelaDreams/VenezuelaDreams/MainViewController.swift
@@ -15,22 +15,36 @@ class MainViewController: UIViewController,UIScrollViewDelegate  {
     
     @IBOutlet weak var rightButton: UIButton!
     @IBOutlet weak var leftButton: UIButton!
-    @IBOutlet weak var backButton: UIButton!
     @IBOutlet weak var donateButton: UIButton!
+    @IBOutlet weak var back: UIButton!
+    @IBOutlet weak var settings: UIButton!
+    
     
     
     @IBOutlet weak var scrollView: UIScrollView!
     var images = [UIImage]()
+    
+
+    var array_pages = [Dictionary<String, String>]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         images = [UIImage(named: "pascal")!,UIImage(named: "jeff")!,UIImage(named: "andres")!]
+        array_pages = getInitialImages()
         setUpScroll()
 
-        loadImages()
-       
+    }
+    
+    func getInitialImages() -> [Dictionary<String, String>] {
+        let profile_one = ["name":"Pascal","bio":"Pascal is 6 years old and likes to play chess and soccer.","image":"pascal"]
+        let profile_two = ["name":"Jeff","bio":"Jeff is 8 years old and likes to play tennis.","image":"jeff"]
+        let profile_three = ["name":"Child","bio":"child child child child child child child child child child child child child child child child child child child child .","image":"pascal"]
         
-        
+        return [profile_one,profile_two,profile_three]
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        loadPages()
     }
 
     func loadImages(){
@@ -49,18 +63,52 @@ class MainViewController: UIViewController,UIScrollViewDelegate  {
         
         scrollView.showsHorizontalScrollIndicator = false
         scrollView.delegate = self
-        
     }
     
-    
-
-
     func setUpScroll(){
-
+        scrollView.isPagingEnabled = true
+        scrollView.contentSize = CGSize(width: self.view.bounds.width * CGFloat(array_pages.count), height: self.scrollView.bounds.height)
+        scrollView.showsHorizontalScrollIndicator = false
+        scrollView.delegate = self
     }
+    
+    func loadPages(){
+        for (index, page) in array_pages.enumerated(){
+            let card = MainCard(frame: CGRect(x: 0, y: 58, width: self.scrollView.bounds.width , height: self.scrollView.bounds.height))
+            card.backgroundColor = UIColor(red: 0, green: 94/255, blue: 112/255, alpha: 1)
+            //card.icon = UIImage(named: "flappy")
+            //card.category = page["name"]!
+            card.categoryLbl.textColor = UIColor.white
+            card.title = page["name"]!
+            card.subtitle = page["bio"]!
+            card.blurEffect = .light
+            //card.itemTitle = page["name"]!
+            //card.itemSubtitle = "Flap That !"
+            card.backgroundImage = UIImage(named: page["image"]!)
+            //card.backgroundColor = UIColor.clear
+            card.textColor = UIColor.white
+            card.hasParallax = true
+            let cardContentVC = storyboard!.instantiateViewController(withIdentifier: "CardContent")
+            card.shouldPresent(cardContentVC, from: self, fullscreen: false)
+            
+            scrollView.addSubview(card)
+            
+            //set origin of x coordinate for the card
+            if (index == 0){
+                card.frame.origin.x = (self.view.bounds.width - self.scrollView.bounds.width) / 2
+            } else {
+                card.frame.origin.x = (CGFloat(index) * self.scrollView.bounds.width) + ((self.view.bounds.width - self.scrollView.bounds.width) / 2)
+            }
+            //set origin of the y coordinate for the card
+            
+            card.frame.origin.y = 0
+        }
+    }
+
+ 
     
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -68,5 +116,17 @@ class MainViewController: UIViewController,UIScrollViewDelegate  {
         // Pass the selected object to the new view controller.
     }
     */
+    @IBAction func goToWelcome(_ sender: Any) {
+        doSegue()
+    }
+    func doSegue(){
+        self.performSegue(withIdentifier: "goToWelcome", sender: self)
+    }
 
+    @IBAction func goToSettings(_ sender: Any) {
+        doSegueSettings()
+    }
+    func doSegueSettings(){
+        self.performSegue(withIdentifier: "toSettings", sender: self)
+    }
 }
